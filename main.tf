0c9bd337d573bdaa39d6e223dae76ddebcc31e42
myname <92176535+Nayeem0030@users.noreply.github.com>
2022-08-24 16:56:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
 region = "us-east-1"
 }
 ##IAM ROLE###
-resource "aws_iam_role" "lamda_role" {
-  name = "terraform_aws_lamda_role"
+resource "aws_iam_role" "lambda_role" {
+  name = "terraform_aws_lambda_role"
   
   assume_role_policy = <<EOF
   {
@@ -14,17 +14,17 @@ resource "aws_iam_role" "lamda_role" {
         Effect = "Allow"
         Sid    = ""
         Principal = {
-          Service = "lamda.amazonaws.com"
+          Service = "lambda.amazonaws.com"
     
         }
       }
     }
 }
 ## Iam policy##
-  resource "aws_iam_policy" "iam_policy_for_lamda" {
-  name        = "aws_iam_policy_for_terraform_aws_lamda_role"
+  resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
   path        = "/"
-  description = "aws policy for managing lamda role"
+  description = "aws policy for managing lambda role"
   policy = <<EOF
     {
     Version = "2012-10-17"
@@ -45,8 +45,8 @@ resource "aws_iam_role" "lamda_role" {
 ##Policy attachment on role ###
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role"
  {
-  role       = aws_iam_role.lamda_role.name
-  policy_arn = aws_iam_policy.iam_policy_for_lamda.arn
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
   #generate an archive from content, file or directory of file#
   data "archive _file" "zip_the_python_code" {
@@ -55,14 +55,14 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role"
   output_path = "${path.module}/python/hello-python.zip"
   }
   
-  ##create lamda function##
+  ##create lambda function##
   
 resource "aws_lambda_function" "terraform_lambda_function" 
 {
   filename      = "${path.module}/python/hello-python.zip"
-  function_name = "Jhooq_lamda_Function"
+  function_name = "Jhooq_lambda_Function"
   role          =  aws_iam_role.lambda_role.arn
-  handler       = "hello-python.lamda_handler"
+  handler       = "hello-python.lambda_handler"
   runtime       = "python3.8"
   depends_on    = "[aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]"
 }
